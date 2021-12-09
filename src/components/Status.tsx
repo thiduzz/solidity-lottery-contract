5b1d0a05dd18ef1bb657cc2e6211515888bdcc31
Thiago <thiduzz14@gmail.com>
2021-12-09 09:11:48 +0100

    Restructure lottery state

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -2,16 +2,28 @@ import { useCallback, useEffect, useState } from 'react'
 import { LotteryContract } from '../contracts/lottery'
 import { web3 } from '../clients/web3'
 
+interface ILotteryState {
+  managerAddress: string
+  joiners: Array<string>
+  balance: string
+}
+
 export const Status = () => {
-  const [manager, setManager] = useState<string>('')
-  const [joinersCount, setJoinersCount] = useState<number>(0)
-  const [balance, setBalance] = useState<string>('')
+  const [lotteryState, setLotteryState] = useState<ILotteryState>({
+    managerAddress: '',
+    joiners: [],
+    balance: '',
+  })
 
   const fetchOwner = useCallback(async () => {
     const joiners = await LotteryContract.methods.getJoiners().call()
-    setManager(await LotteryContract.methods.manager().call())
-    setJoinersCount(joiners.length)
-    setBalance(await web3.eth.getBalance(LotteryContract.options.address))
+    const balance = await web3.eth.getBalance(LotteryContract.options.address)
+    const managerAddress = await LotteryContract.methods.manager().call()
+    setLotteryState({
+      managerAddress,
+      joiners,
+      balance,
+    })
   }, [])
 
   useEffect(() => {
@@ -22,13 +34,13 @@ export const Status = () => {
     <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
       <h1 className="text-xl font-black text-white text-center">
         <p className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
-          Manager: {manager}
+          Manager: {lotteryState.managerAddress}
         </p>
         <p className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
-          Joiners: {joinersCount}
+          Joiners: {lotteryState.joiners.length}
         </p>
         <p className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
-          Lottery Pot: {web3.utils.fromWei(balance, 'ether')} ether
+          Lottery Pot: {web3.utils.fromWei(lotteryState.balance, 'ether')} ether
         </p>
       </h1>
     </div>
