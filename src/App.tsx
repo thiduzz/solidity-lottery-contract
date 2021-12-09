8576670749175daba2231753bcb1eb22dab725d4
Thiago <thiduzz14@gmail.com>
2021-12-09 09:06:45 +0100

    Remove unecessary dependency and retrieve more info

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import './App.scss'
 import { Status } from './components/Status'
 import Logo from './components/Logo'
 import Menu from './components/Menu'
-import Web3Client from './clients/web3'
 
 const App = () => {
   const topbarClasses = 'top-bar'
-
-  const client = new Web3Client()
 
   return (
     <div className="App">
@@ -15,7 +12,7 @@ const App = () => {
         <Logo />
         <Menu />
       </div>
-      <Status client={client} />
+      <Status />
     </div>
   )
 }
--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,23 +1,18 @@
 import { useCallback, useEffect, useState } from 'react'
-import Web3Client from '../clients/web3'
 import { LotteryContract } from '../contracts/lottery'
+import { web3 } from '../clients/web3'
 
-type Props = {
-  client: Web3Client
-}
-
-export const Status = ({ client }: Props) => {
+export const Status = () => {
   const [manager, setManager] = useState<string>('')
+  const [joinersCount, setJoinersCount] = useState<number>(0)
+  const [balance, setBalance] = useState<string>('')
 
   const fetchOwner = useCallback(async () => {
-    // eslint-disable-next-line
-    console.log(LotteryContract.options.address)
-    const response = await LotteryContract.methods.manager().call()
-    setManager(response)
+    const joiners = await LotteryContract.methods.getJoiners().call()
+    setManager(await LotteryContract.methods.manager().call())
+    setJoinersCount(joiners.length)
+    setBalance(await web3.eth.getBalance(LotteryContract.options.address))
   }, [])
-
-  // eslint-disable-next-line
-  console.log(client)
 
   useEffect(() => {
     fetchOwner()
@@ -26,9 +21,15 @@ export const Status = ({ client }: Props) => {
   return (
     <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700">
       <h1 className="text-xl font-black text-white text-center">
-        <span className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
-          {manager}
-        </span>
+        <p className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+          Manager: {manager}
+        </p>
+        <p className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+          Joiners: {joinersCount}
+        </p>
+        <p className="bg-gradient-to-r text-transparent bg-clip-text from-green-400 to-purple-500">
+          Lottery Pot: {web3.utils.fromWei(balance, 'ether')} ether
+        </p>
       </h1>
     </div>
   )
