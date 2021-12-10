1f891e3159dc767f74687297b5cb625a5c4b3736
Thiago <thiduzz14@gmail.com>
2021-12-10 08:35:00 +0100

    Wrap components into provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import { Status } from './components/Status'
 import Logo from './components/Logo'
 import Menu from './components/Menu'
 import { JoinForm } from './components/JoinForm'
+import { LotteryProvider } from './context/LotteryContext'
 
 const App = () => {
   const topbarClasses = 'top-bar'
 
   return (
     <div className="App">
-      <div className={topbarClasses}>
-        <Logo />
-        <Menu />
-      </div>
+      <LotteryProvider>
+        <div className={topbarClasses}>
+          <Logo />
+          <Menu />
+        </div>
 
-      <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700 flex-col">
-        <Status />
-        <JoinForm />
-      </div>
+        <div className="p-10 min-h-screen flex items-center justify-center bg-cool-gray-700 flex-col">
+          <Status />
+          <JoinForm />
+        </div>
+      </LotteryProvider>
     </div>
   )
 }
