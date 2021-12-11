13e8afa1428cf12c2eb924b290e07ae4d0b9f0c9
Thiago <thiduzz14@gmail.com>
2021-12-11 10:18:50 +0100

    Animate Flash Message with Fade in/out and add timing logic

--- src/components/FlashMessage.tsx
+++ src/components/FlashMessage.tsx
@@ -1,22 +1,61 @@
+import { useEffect } from 'react'
+import { IoCloseCircle } from 'react-icons/io5'
+import { TransitionGroup, Transition } from 'react-transition-group'
 import { useFlashMessage } from '../context/FlashMessageContext'
 import './FlashMessage.scss'
 
 const FlashMessage = () => {
-  const { visible, current } = useFlashMessage()
+  const { visible, current, close } = useFlashMessage()
 
-  if (!visible || !current) {
-    return null
+  useEffect(() => {
+    if (current && current.timerInSeconds) {
+      const interval = setInterval(close, 1000 * current.timerInSeconds)
+      return () => clearInterval(interval)
+    }
+    return () => {}
+  }, [current, close])
+
+  const transitionStyles = {
+    entering: { opacity: 0.01 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
   }
+
+  const messageType = current?.type ?? ''
+  const shouldShow = visible && current !== null
+  console.log(shouldShow)
   return (
-    <div className={`flash-message ${current.type}`}>
-      <div>
-        <h3 className="">{current.title}</h3>
-        {current.message && <span>{current.message}</span>}
-      </div>
-      <div>
-        <button type="button">x</button>
-      </div>
-    </div>
+    <TransitionGroup>
+      {shouldShow && (
+        <Transition timeout={{ enter: 300, exit: 300 }}>
+          {(state) => (
+            <div
+              className={`flash-message ${messageType}`}
+              style={{ ...transitionStyles[state] }}
+            >
+              {current != null && (
+                <div className="flash-message-container">
+                  <div>
+                    <h3>{current.title}</h3>
+                    {current && current.message && (
+                      <span>{current.message}</span>
+                    )}
+                  </div>
+                  {current && current.closeable && (
+                    <div className="close-btn-container">
+                      <button type="button" onClick={close}>
+                        <IoCloseCircle />
+                      </button>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          )}
+        </Transition>
+      )}
+    </TransitionGroup>
   )
 }
 
