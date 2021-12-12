d951be8b4b6a3e1285027a1268f96c0528f464e1
Thiago <thiduzz14@gmail.com>
2021-12-12 17:05:28 +0100

    Extend usage of Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,27 @@
-export const Loading = () => (
-  <div className="flex justify-center items-center h-screen">
-    <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white" />
+import './Loading.scss'
+
+export enum LoadingSizes {
+  'SMALL' = 'small',
+  'MEDIUM' = 'medium',
+  'LARGE' = 'large',
+  'FULLSCREEN' = 'fullscreen',
+}
+
+export enum LoadingTypes {
+  'GRADIENT' = 'gradient',
+  'PLAIN' = 'plain',
+}
+
+interface ILoadingProps {
+  type: LoadingTypes
+  size: LoadingSizes
+  className?: string
+}
+
+export const Loading: React.FC<ILoadingProps> = ({ type, size, className }) => (
+  <div className={`loading-spinner ${className} ${type} ${size}`}>
+    <div className="spinner-container">
+      <div className="spinner" />
     </div>
   </div>
 )
